b769c8e753b6bdb325fa70595ae7bfb362b02aa5
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-03-24 23:46:52 +0100

    Using viewsets which combine CRUD and ListView actions

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,30 +1,38 @@
-from rest_framework.generics import (
-  ListAPIView, 
-  RetrieveAPIView,
-  CreateAPIView,
-  UpdateAPIView,
-  DestroyAPIView
-)
-
 from articles.models import Article
 from .serializers import ArticleSerializer
 
-class ArticleListView(ListAPIView):
-  queryset = Article.objects.all()
-  serializer_class = ArticleSerializer
+from rest_framework import viewsets
 
-class ArticleDetailView(RetrieveAPIView):
-  queryset = Article.objects.all()
-  serializer_class = ArticleSerializer
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 
-class ArticleCreateView(CreateAPIView):
-  queryset = Article.objects.all()
-  serializer_class = ArticleSerializer
 
-class ArticleUpdateView(UpdateAPIView):
-  queryset = Article.objects.all()
-  serializer_class = ArticleSerializer
+# from rest_framework.generics import (
+#   ListAPIView, 
+#   RetrieveAPIView,
+#   CreateAPIView,
+#   UpdateAPIView,
+#   DestroyAPIView
+# )
 
-class ArticleDeleteView(DestroyAPIView):
-  queryset = Article.objects.all()
-  serializer_class = ArticleSerializer
+
+# class ArticleListView(ListAPIView):
+#   queryset = Article.objects.all()
+#   serializer_class = ArticleSerializer
+
+# class ArticleDetailView(RetrieveAPIView):
+#   queryset = Article.objects.all()
+#   serializer_class = ArticleSerializer
+
+# class ArticleCreateView(CreateAPIView):
+#   queryset = Article.objects.all()
+#   serializer_class = ArticleSerializer
+
+# class ArticleUpdateView(UpdateAPIView):
+#   queryset = Article.objects.all()
+#   serializer_class = ArticleSerializer
+
+# class ArticleDeleteView(DestroyAPIView):
+#   queryset = Article.objects.all()
+#   serializer_class = ArticleSerializer
