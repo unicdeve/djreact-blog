83f2b8c5e3fb7e8a75885eb1c9fda8a0c8fd71e5
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-03-24 03:26:33 +0100

    Ant design for the Articles Layout

---
+++ frontend/gui/src/containers/ArticleListView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Article from '../components/Article';
+
+export default class ArticleList extends Component {
+  render() {
+    return (
+      <Article />
+    )
+  }
+}
