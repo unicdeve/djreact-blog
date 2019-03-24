84bdb8bced684d82f5ea91a547e8c6a9419ed8d0
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-03-24 02:43:49 +0100

    articles app, models and api

--- backend/src/articles/api/serializers.py
+++ backend/src/articles/api/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 from articles.models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
-  class class Meta:
+  class Meta:
     model = Article
-    fields = ('title', 'content')
+    fields = ('name', 'content')
     verbose_name = 'Article'
     verbose_name_plural = 'Articles'
